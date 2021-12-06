e6a3eca5037e3c80c3b9fd3a9f2b4f07b2c38676
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-06 17:26:38 +0900

    itemsコントローラーのnewアクションのビューファイル編集、ActiveHashの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,22 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params_item)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def params_item
+      params.require(:item).permit(:item_name, :describe, :category_id, :status_id, :delivery_fee_load_id, :prefecture_id, :sending_date_id, :price).merge(:user, user_id: current_user.id)
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :item_name
+    validates :describe
+    validates :price
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_fee_load_id
+      validates :prefecture_id
+      validates :sending_date_id
+    end
+  end
+
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
