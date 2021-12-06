f07b362c05ee69efb3add039b94a3ae89e7443b7
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-07 08:13:06 +0900

    Rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   end
 
   private
-    def params_item
-      params.require(:item).permit(:item_name, :describe, :category_id, :status_id, :delivery_fee_load_id, :prefecture_id, :sending_date_id, :price, :image).merge(user_id: current_user.id)
-    end
+  def params_item
+    params.require(:item).permit(:item_name, :describe, :category_id, :status_id, :delivery_fee_load_id, :prefecture_id, :sending_date_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
