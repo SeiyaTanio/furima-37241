ee2e9ca75b31f5ba114972f8a2d6b28da9d22a65
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-06 11:27:47 +0900

    ストロングパラメーター、バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -8,5 +9,14 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, key: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, key: [:kanji_family_name])
+    devise_parameter_sanitizer.permit(:sign_up, key: [:kanji_first_name])
+    devise_parameter_sanitizer.permit(:sign_up, key: [:kana_family_name])
+    devise_parameter_sanitizer.permit(:sign_up, key: [:kana_first_name])
+    devise_parameter_sanitizer.permit(:sign_up, key: [:birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :kanji_family_name, presence: true
+  validates :kanji_first_name, presence: true
+  validates :kana_family_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :birth_date, presence: true
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
