db09e69b2d712eaa7cf96c7bf2e36d0d6da31069
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-06 17:46:51 +0900

    販売価格300~9,999,999のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :describe
-    validates :price
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
       validates :status_id
@@ -11,6 +10,10 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :sending_date_id
     end
+  end
+  with_options presence: true, format: { with: /A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank" }
   end
 
   belongs_to :user
