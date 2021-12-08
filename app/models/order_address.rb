8a653ddf7311f5cbdde2432f740675a0f8770582
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-08 11:48:25 +0900

    コントローラー、モデルの作成、フォームオブジェクトパターンでの実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :tel_number, :order_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :municipalities
+    validates :house_number
+    validates :tel_number, numericality: {only_integer: true, message: 'is invalid'}, format: {with: /\A\d{10,11}\z/}
+    validates :order_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, tel_number: tel_number, order_id: order.id)
+  end
+end
